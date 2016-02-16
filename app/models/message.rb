66d2309dc8aa2a21219ef63f4c1c5a25bfa5fb94
Alexander Andreev <cinic.rus@gmail.com>
2016-02-16 04:24:34 +0300

    Refactor code.
    Remove unused method to rake task for whenever

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,4 @@ class Message < ActiveRecord::Base
   has_many :recipients
 
   validates :postfix_queue_id, presence: true
-
-  def decode_log
-    parser = Parser::MailLog.new Rails.root.join('log', 'mail.log')
-    parser.parse_log
-  end
 end
