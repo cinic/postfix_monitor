92a50a9411c65e55cdc3883f47b70e81e4c4a88b
Alexander Andreev <cinic.rus@gmail.com>
2016-02-28 00:06:13 +0300

    Add delivered/undelivered views

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ActiveRecord::Base
   has_many :recipients
 
   validates :postfix_queue_id, presence: true
+
+  def sended
+    created_at - 1.day
+  end
 end
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,5 +1,11 @@
 class Recipient < ActiveRecord::Base
   belongs_to :message
 
+  scope :delivered, -> { where(delivered: true).count }
+  scope :delivered_list, -> { where(delivered: true) }
+  scope :undelivered, -> { where(delivered: false).count }
+  scope :undelivered_list, -> { where(delivered: false) }
+
   validates :address, :status, presence: true
+
 end
